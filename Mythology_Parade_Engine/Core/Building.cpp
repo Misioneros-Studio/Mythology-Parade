b06c47dcd4731a13f9268abba63d64cb54465e1e
jordi-pardo <jordipardogutierrez@gmail.com>
2020-03-24 11:33:46 +0100

    Update Building.cpp

--- Mythology_Parade_Engine/Core/Building.cpp
+++ Mythology_Parade_Engine/Core/Building.cpp
@@ -1,7 +1,21 @@
 #include "Building.h"
 
-Building::Building(BuildingType)
+Building::Building(BuildingType buildingType)
 {
+	switch (buildingType)
+	{
+	case FORTRESS:
+		break;
+	case MONASTERY:
+		break;
+	case TEMPLE:
+		break;
+	case ENCAMPMENT:
+		break;
+	default:
+		break;
+	}
+
 	SetMaxHealth(300);
 	description = "hola";
 }
