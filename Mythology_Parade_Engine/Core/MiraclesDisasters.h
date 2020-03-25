b4d33d7880ab4d12f42ae6cba60269f93e8e10ac
Bernat Casanas <bernatcasanas@gmail.com>
2020-03-25 08:25:52 +0100

    defibnint disasters and miracles

---
+++ Mythology_Parade_Engine/Core/MiraclesDisasters.h
@@ -0,0 +1,33 @@
+#ifndef _MIRACLESDISASTERS_
+#define _MIRACLESDISASTERS_
+
+
+class MiraclesDisasters 
+{
+public:
+	enum class Miracles {
+		ClericEnlightment,
+		BordersPressure,
+		BeastTame,
+		MassConversion,
+		ForcedTraition,
+		WeakenedBorders,
+		CallToArms,
+		GoldenExpansion,
+		Victory
+	};
+	enum class Disaster {
+		MilitaryPressure,
+		TroopsInspiration,
+		HolyMeteor,
+		StrikingFear,
+		BloodFlow,
+		TheresRoom,
+		ColdAssassination,
+		AnimalShelter,
+		Victory
+	};
+};
+
+
+#endif // !_MIRACLESDISASTERS_
