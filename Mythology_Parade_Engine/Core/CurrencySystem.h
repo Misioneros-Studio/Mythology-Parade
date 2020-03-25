7999f5c67c5bad45980df4d6a687e39438486cd3
Bernat Casanas <bernatcasanas@gmail.com>
2020-03-25 19:40:24 +0100

    modifications (testing)

--- Mythology_Parade_Engine/Core/CurrencySystem.h
+++ Mythology_Parade_Engine/Core/CurrencySystem.h
@@ -5,6 +5,7 @@
 class CurrencySystem
 {
 public:
+	//ECONOMY BASICS
 	enum class ActionSacrifice {
 		RivalKilled,
 		EliteKilled,
@@ -21,17 +22,46 @@ public:
 		BeastConverted,
 		MonasteriesPrayers
 	};
+	//MIRACLES AND DISASTRES
+	enum class Miracles {
+		ClericEnlightment,
+		BordersPressure,
+		BeastTame,
+		MassConversion,
+		ForcedTraition,
+		WeakenedBorders,
+		CallToArms,
+		GoldenExpansion,
+		Victory
+	};
+	enum class Disasters {
+		MilitaryPressure,
+		TroopsInspiration,
+		HolyMeteor,
+		StrikingFear,
+		BloodFlow,
+		TheresRoom,
+		ColdAssassination,
+		AnimalShelter,
+		Victory
+	};
 
 public:
 	CurrencySystem();
 	~CurrencySystem();
 
+	//BASIC ECONOMY FUNCTONS
 	void increaseFaith(int number);
 	void decreaseFaith(int number);
 	void increaseSacrifice(ActionSacrifice action);
 	void increasePrayers(ActionPrayers action);
 
+	//MIRACLES AND DISASTER
+	void Miracle(Miracles action, CurrencySystem* currency);
+	void Disaster(Disasters action, CurrencySystem* currency);
+
 public:
+	//ECONOMY
 	int sacrifices;
 	int faith;
 	int prayers;
