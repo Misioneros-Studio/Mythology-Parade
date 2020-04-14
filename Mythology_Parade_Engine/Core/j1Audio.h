96b9830f0cb731336fd99650c1b4a0fac85b9b3d
Ramsubito <47743770+Ramsubito@users.noreply.github.com>
2020-04-14 13:59:27 +0200

    Revert "Merge branch 'Develop' of https://github.com/Misioneros-Studio/Mythology-Parade into Develop"
    This reverts commit 066774c3090d3ee0eb7bc9a7c773c4014a8b2722, reversing
    changes made to fb3c09f957afcd0eb925f28db91412302c72f4d6.

--- Mythology_Parade_Engine/Core/j1Audio.h
+++ Mythology_Parade_Engine/Core/j1Audio.h
@@ -2,10 +2,8 @@
 #define __j1AUDIO_H__
 
 #include "j1Module.h"
-#include "SDL_mixer\include\SDL_mixer.h"
 
 #define DEFAULT_MUSIC_FADE_TIME 2.0f
-#define MAX_VOLUME 128
 
 struct _Mix_Music;
 struct Mix_Chunk;
@@ -26,23 +24,16 @@ public:
 	bool CleanUp();
 
 	// Play a music file
-	bool PlayMusic(const char* path, float fade_time = DEFAULT_MUSIC_FADE_TIME, int volume = 200);
+	bool PlayMusic(const char* path, float fade_time = DEFAULT_MUSIC_FADE_TIME);
 
 	// Load a WAV in memory
 	unsigned int LoadFx(const char* path);
 
 	// Play a previously loaded WAV
-	bool PlayFx(int channel , unsigned int fx, int repeat = 0);
+	bool PlayFx(unsigned int fx, int repeat = 0);
 
 	// Clean all fxs to change scene
 	bool CleanFxs();
-
-	uint WinVikings_sound;
-	uint WinGreeks_sound;
-	uint Lose_Sound;
-	uint NewGame_transition;
-	uint ExitGame_transition;
-	uint SeaSound;
 
 private:
 
