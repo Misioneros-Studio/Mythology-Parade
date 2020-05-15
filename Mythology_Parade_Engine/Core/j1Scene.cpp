998e75d2ca6a28607a5a6affea78784a59462b1d
Arnau77 <47742576+Arnau77@users.noreply.github.com>
2020-05-15 17:58:47 +0200

    solve bug slider audio scene

--- Mythology_Parade_Engine/Core/j1Scene.cpp
+++ Mythology_Parade_Engine/Core/j1Scene.cpp
@@ -254,14 +254,14 @@ bool j1Scene::Update(float dt)
 	if (App->input->GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN) {
 		hud->ActivatePauseMenu();
 		App->audio->FadeAudio(which_audio_fade::change_volume, 2, 50);
-
+	}
     if (paused_game == true) {
       if (hud->ui_volume_sliders[0] != nullptr)
         hud->UpdateSlider(0);
       if (hud->ui_volume_sliders[3] != nullptr)
         hud->UpdateSlider(3);
     }
-	}
+	
 
 	SDL_Rect correctedCamera = App->render->camera;
 	correctedCamera.x = -correctedCamera.x;
